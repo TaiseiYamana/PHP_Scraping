1949c7f29c7859a8e8591a388796810e013b7820
TaiseiYamana <mit.yamatai@gmail.com>
2021-06-02 02:57:01 +0900

    remouve:coment out cloum

--- Scrayping.php
+++ Scrayping.php
@@ -13,15 +13,6 @@ $list_url = "https://www.nii.ac.jp/faculty/list/professors/";
 $html_1 = file_get_contents($list_url);
 $dom_1 = phpQuery::newDocument($html_1);
 
-//$name = $dom_1->find('#contentBox')->find('ul　>　li')->text();
-//$url = $dom->find('#contentBox')->find('ul　>　li > a')->attr("href");
-
-//$name = $dom->find("#contentBox > ur > li > a") ->text();
-//$name = $dom->find('ur > li > a')->text();
-#$url = $dom
-
-//echo $name;
-
 foreach ($dom_1->find('#contentBox')->find('ul > li > a') as $a){
   $name = $a->textContent;
   $url = $a->getAttribute('href');
@@ -33,51 +24,12 @@ foreach ($dom_1->find('#contentBox')->find('ul > li > a') as $a){
   $html_2 = file_get_contents($parson_url);
   $dom_2 = phpQuery::newDocument($html_2);
 
-  //$alltitle = $dom_2->find('#contentBox')->find('div');
-  //$alltitle = $dom_2->find('#contentBox')->find('div');
   $alltitle = $dom_2->find('.bg-white.padding-all-md.bd-gray-bottom.margin-bottom-default.text-default.text-sm-sp.cf');
   $alltitle->find('span')->remove();
-  //$dom_3->find('a')->remove();
-
-  //foreach ($dom_3 as $b){
-  //echo $b->textContent . '<br>';
-  //}
 
   echo $alltitle->text() . '<br>';
-  /*
-  foreach ($dom_4 as $c){
-    $url2 = $c->getAttribute('href');
-    echo 'url:' . $url2 . '<br>';
-  }
-  */
-
-  //$content = $dom_2->find('.margin-top-default')->text();
-  //$content = $dom_2->find("h1")->text();
-  #$img = $dom_2->find("#bg-white padding-all-md bd-gray-bottom margin-bottom-default text-default text-sm-sp cf")->find('img');
-  #$img = $img->getAttribute('src');
-  #echo  '<img src="' . $top_url . $img . '"><br>';
-  //echo $content . '<br>';
 }
 
-
-//echo "¥n";
-//echo $url;
-/*
-//aタグの一覧を取得
-foreach ($dom->find('ul')->find('li')->find('a') as $a){
-  $name = $a->getText();
-  echo $name;
-}
-
-https://www.nii.ac.jp/faculty/digital_content/aihara_kenro/
-/*
-foreach ($dom->find('img') as $img){
-  $img = $img->getAttribute('src');
-  echo '<img src=' . $img . '><br>';
-}
-*/
-
-//echo phpQuery::newDocument($html)->find("h3")->text();
 
 
 ?>
